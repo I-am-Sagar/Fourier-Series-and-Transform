80152552ec8b42facccdae66888e96d1fcf38e70
I-am-Sagar <sagar.udasi.official@gmail.com>
2020-03-01 18:05:02 +0530

    Now the final work will go into DSP final

---
+++ Old stuff/Animations/Fourier Series Basic.js
@@ -0,0 +1,55 @@
+let time = 0;
+let wave = [];
+let slider;
+
+function setup() {
+    let cnv = createCanvas(600, 400);
+    cnv.style('display', 'block');
+    cnv.parent('canvas');
+    slider = createSlider(1, 10, 1);
+    slider.parent('control');
+}
+
+function windowResized() {
+    resizeCanvas(600,400);
+}
+
+function draw() {
+    background(0);
+    translate(150, 200);
+
+    let x = 0, y = 0;
+    
+    for (let i = 0; i < slider.value(); i++) {
+        let prevx = x;
+        let prevy = y;
+
+        let n = 2*i+1;
+        let radius = 75*(4/(n*PI));
+        x += radius*cos(n*time);
+        y += radius*sin(n*time);
+
+        stroke(255, 100);
+        noFill();
+        ellipse (prevx, prevy, radius*2);
+
+        stroke(255);
+        line(prevx, prevy, x, y);
+    }
+
+    wave.unshift(y);
+    translate(200, 0);
+    line(x-200, y, 0, wave[0]);
+    beginShape();
+    noFill();
+    for (let i = 0; i < wave.length; i++) {
+        vertex(i, wave[i]);
+    }
+    endShape();
+
+    time += 0.03;
+
+    if (wave.length > 250){
+        wave.pop();
+    }
+}   
---
+++ Old stuff/Animations/fourier.js
@@ -0,0 +1,24 @@
+function dft (x) {
+    const X = [];
+    const N = x.length;
+    for (let k = 0; k < N; k++) {
+        let real = 0;
+        let imag = 0;
+
+        for (let n = 0; n < N; n++) {
+            const phi = (TWO_PI*k*n)/N; 
+            real += x[n]*cos(phi);
+            imag -= x[n]*sin(phi);
+        }
+        real = real/N;
+        imag = imag/N;
+        
+        let freq = k;
+        let amp = sqrt(real*real + imag*imag);
+        let phase = atan2(imag, real);
+    
+        X[k] = { real, imag, freq, amp, phase };
+    }
+
+    return X;
+}
